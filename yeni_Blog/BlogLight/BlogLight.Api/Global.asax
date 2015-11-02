82389b57f20fed65064724b95efe8c9af8eb62af
Fatma Koç <fatmakoc94@gmail.com>
2015-11-02 17:26:06 +0200

    Revert "Web Api & Angular JS"
    This reverts commit 289ad0201d3cc3a029eff89d5d75b4b39d4246cc.

--- yeni_Blog/BlogLight/BlogLight.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlogLight.Api.WebApiApplication" Language="C#" %>
--- yeni_Blog/BlogLight/BlogLight.Api/app/ContactController.js
+++
@@ -1,8 +0,0 @@
-﻿var app = angular.module('MyApp');
-
-app.controller('ContactCtrl', function ($scope) {
-    $scope.header = "İLETİŞİM",
-    $scope.sosyal = "http://facebook.com/someone",
-    $scope.mail = "biri@someone.com",
-    $scope.mesaj = "Mesaj"
-});
